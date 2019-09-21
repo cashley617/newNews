d192ae9002a73359834492693ee56b8e506b678b
Cate Ashley <catemarie.ashley@gmail.com>
2019-09-21 10:08:27 -0600

    updated article.js

---
+++ models/Article.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+// Schema constructor 
+let Schema = mongoose.Schema; 
+
+// New UserSchema 
+let ArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+
+    link: { 
+        type: String,
+        required: true
+    },
+
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+});
+
+let Article = mongoose.model("Article", ArticleSchema);
+
+// Export
+module.exports = Article; 
