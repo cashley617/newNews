306133d045f708c48e442e7f6c0cd36917306cba
Cate Ashley <catemarie.ashley@gmail.com>
2019-09-22 08:24:04 -0600

    updated css

--- server.js
+++ server.js
@@ -1,144 +1,3 @@
-// // List of dependencies 
-// const express = require('express'),
-//     mongoose = require('mongoose'),
-//     exphbs = require('express-handlebars'),
-//     bodyParser = require('body-parser'),
-//     logger = require('morgan'),
-//     path = require('path'),
-//     favicon = require('serve-favicon');
-
-// // Scraping 
-// const cheerio = require('cheerio'),
-//     // request = require('request');
-//     axios = require('axios');
-
-// // Require models 
-// const db = require('./models');
-
-// // Initialize app 
-// const PORT = process.env.PORT || 3000;
-// const app = express(); 
-
-// // Configure the DB 
-// // const config = require('./config/database');
-// //     mongoose.Promise = Promise;
-// //     mongoose.connect(config.database)
-// //         .then( result => {
-// //             console.log('Connected to db ' ${result.connections[0].name}' on${result')
-// //         })
-
-// // Configure Favicon middleware 
-// // app.use(favicon(path.join(__dirname, 'public', 'assets/img/favicon.ico')));
-
-// // Configure the Morgan middleware 
-// app.use(logger('dev'));
-
-// // Body-parser for forms 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// // Handlebars
-// app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-// app.set('view engine', 'handlebars');
-
-// // Serves public folder for static directory 
-// // app.use(express.static(path.join(__dirname, '/public')));
-// app.use(express.static("public"));
-// app.use('articles', express.static("public"));
-// app.use('/notes', express.static("public"));
-
-
-// // app.use('/articles', express.static(path.join(_dirname, '/public')));
-// // app.use('/notes', express.static(path.join(_dirname, '/public')));
-
-// // Connect to Mongo DB 
-// mongoose.connect("mongodb://localhost/newNews", {
-//   useNewUrlParser: true
-// });
-
-
-// // Routes 
-// // const index = require('./routes/index'),
-// //     articles = require('./routes/articles'),
-// //     notes = require('./routes/notes'),
-// //     scrape = require('./routes/scrape'); 
-
-// // app.use('/', index);
-// // app.use('/articles', articles);
-// // app.use('/notes', notes);
-// // app.use('/scrape', scrape); 
-
-// // GET Route to scrape 
-// app.get('/scrape', function(req, res) { 
-//     axios.get("https://www.aljazeera.com/").then(function(response) {
-//         let $ = cheerio.load(response.data); 
-
-//         // Secure h2 sections in article 
-//         $('article h2').each(function(i, element) {
-//             let result = {}; 
-
-//             result.title = $(this) 
-//                 .children('a')
-//                 .text(); 
-//             result.link = $(this) 
-//                 .children('a')
-//                 .attr("href");
-
-//             // New Article 
-//             db.Article.create(result) 
-//                 .then(function(dbArticle) {
-//                     console.log(dbArticle)
-//                 })
-//                 .catch(function(err) { 
-//                     console.logg(err)
-//                 });
-//         });
-
-//         res.send("Your scrape is done!");
-//     });
-// });
-
-// // Route to get Articles 
-// app.get('/articles', function(req, res) {
-//     db.Article.find({})
-//         .then(function(dbArticle) {
-//             res.json(dbArticle);
-//         })
-//     .catch(function(err) {
-//         res.json(err);
-//     });
-// });
-
-// // Route to get Article by id
-// app.get('/articles/:id', function(req, res) { 
-//     db.Article.findOne({_id: req.params.id })
-//         .populate('note')
-//         .then(function(dbArticle) { 
-//             res.json(dbArticle);
-//         })
-//         .catch(function(err) { 
-//             res.json(err);
-//         });
-// });
-
-// // Route to save Article note 
-// app.post('/articles/:id', function(req, res) { 
-//     db.Note.create(req.body) 
-//         .then(function(dbNote) { 
-//             return db.Article.findOneAndUpdate({_id: req.params.id }, { note: dbNote._id }, { new: true });
-//         })
-//         .then(function(dbArticle) { 
-//             res.json(dbArticle);
-//         })
-//         .catch(function(err) { 
-//             res.json(err);
-//         });
-// });
-
-// // Server 
-// app.listen(PORT, function() {
-//     console.log('Listening on PORT: ' + PORT);
-// });
 
 var express = require("express");
 var logger = require("morgan");
@@ -170,48 +29,138 @@ app.use(express.static("public"));
 
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+    // var databaseUrl = "news";
+    // mongoose.Promise = Promise;
+    // var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/news";
+    // mongoose.connect(MONGODB_URI);
 
 // Routes
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function (req, res) {
     // First, we grab the body of the html with axios
-    axios.get("https://www.aljazeera.com").then(function(response) {
-      // Then, we load that into cheerio and save it to $ for a shorthand selector
-      var $ = cheerio.load(response.data);
+    axios.get("http://www.echojs.com/").then(function (response) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(response.data);
 
-      // Now, we grab every h2 within an article tag, and do the following:
-      $("article h2").each(function(i, element) {
-        // Save an empty result object
-        var result = {};
+        // Now, we grab every h2 within an article tag, and do the following:
+        $("article h2").each(function (i, element) {
+            // Save an empty result object
+            var result = {};
 
-        // Add the text and href of every link, and save them as properties of the result object
-        result.title = $(this)
-          .children("a")
-          .text();
-        result.link = $(this)
-          .children("a")
-          .attr("href");
-        result.image = $(this)
-          .children("source")
-          .attr("data-srcset");
+            // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
 
-        // Create a new Article using the `result` object built from scraping
-        db.Article.create(result)
-          .then(function(dbArticle) {
-            // View the added result in the console
-            console.log(dbArticle);
-          })
-          .catch(function(err) {
-            // If an error occurred, log it
-            console.log(err);
-          });
-      });
+            // Create a new Article using the `result` object built from scraping
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    // View the added result in the console
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    // If an error occurred, log it
+                    console.log(err);
+                });
+        });
 
-      // Send a message to the client
-      res.send("Scrape Complete");
+        // Send a message to the client
+        res.send("Scrape Complete");
     });
 });
+
+
+
+// app.get("/scrape", function (req, res) {
+//     // First, we grab the body of the html with axios
+//     axios.get("https://www.aljazeera.net/news").then(function(response) {
+//       // Then, we load that into cheerio and save it to $ for a shorthand selector
+//       let $ = cheerio.load(response.data);
+
+//       // Now, we grab every h2 within an article tag, and do the following:
+//       $("h2").each(function(i, element) {
+//         // Save an empty result object
+//         var result = {};
+
+//         // Add the text and href of every link, and save them as properties of the result object
+//         result.title = $(element)
+//             .find("textContent")
+//             .text()
+//             .text();
+//         result.innerHTML = $(element)
+//             .find("innterHTML")
+//             .attr("href");
+//         result.image = $(element)
+//             .children("source")
+//             .attr("data-srcset");
+
+//         // Create a new Article using the `result` object built from scraping
+//         db.Article.create(result)
+//           .then(function(dbArticle) {
+//             // View the added result in the console
+//             console.log(dbArticle);
+//           })
+//           .catch(function(err) {
+//             // If an error occurred, log it
+//             console.log(err);
+//           });
+//       });
+
+//       // Send a message to the client
+//       res.send("Scrape Complete");
+//     });
+// });
+
+// app.get("/scrape", function (req, res) {
+//     axios.get("https://www.aljazeera.net/news").then(function (response) {
+//       // Load the html body from request into cheerio
+//       var $ = cheerio.load(response.data);
+//       $("h2").each(function(i, element) {
+//         // trim() removes whitespace because the items return \n and \t before and after the text
+//         var title = $(element)
+//           .find("textContent")
+//           .text()
+//           .trim();
+//         var link = $(element)
+//           .find("innterHTML")
+//           .attr("href");
+//         // var intro = $(element)
+//         //   .children(".post-block__content")
+//         //   .text()
+//         //   .trim();
+
+//         // if these are present in the scraped data, create an article in the database collection
+//         if (title && link && intro) {
+//           db.Article.create(
+//             {
+//               title: title,
+//               link: link,
+//             //   intro: intro
+//             },
+//             function(error, inserted) {
+//               if (error) {
+//                 // log the error if one is encountered during the query
+//                 console.log(error);
+//               } else {
+//                 // otherwise, log the inserted data
+//                 console.log(inserted);
+//               }
+//             }
+//           );
+//           // if there are 10 articles, then return the callback to the frontend
+//           console.log(i);
+//           if (i === 10) {
+//             return res.sendStatus(200);
+//           }
+//         }
+//       });
+//     });
+// });
+
 
 // Route for getting all Articles from the db
 app.get("/articles", function (req, res) {
